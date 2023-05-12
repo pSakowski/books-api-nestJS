112761d3f567d892da35b493d4843ee0daac8d5f
pSakowski <p.sakowski1213@gmail.com>
2023-05-12 18:50:31 +0200

    add global validation pipe to app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './shared/services/prisma.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
 
   const prismaService = app.get(PrismaService);
