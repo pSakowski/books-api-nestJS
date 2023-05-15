40db2be48b9f90537aa7945cd746aeb0b0f222b0
pSakowski <p.sakowski1213@gmail.com>
2023-05-15 13:24:45 +0200

    install cookie-parser package & add it as middleware

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
