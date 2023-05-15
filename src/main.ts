b919fb7146fb8dc30513f13ca4f3cae8c9f93601
pSakowski <p.sakowski1213@gmail.com>
2023-05-15 14:32:01 +0200

    implement config management with @nestjs/config for secure JWT secret storage

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('port'));
 }
 bootstrap();
